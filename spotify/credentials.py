5277096fb493115de43bbe60d8a95251c5ca7fc3
static08 <74912169+static08@users.noreply.github.com>
2021-04-23 14:54:34 +0530

    Update credentials.py

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "d614646b0c2a43ed94403ab2aaaa34d5"
-CLIENT_SECRET = "0ffd09607ca244d8b81a77862e4025c3"
+CLIENT_SECRET = "jahdakhdla3" // This has to be kept secret 
 REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
